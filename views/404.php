a9aa2d24fd1c53d3e3f092473c4d3b277aeba2f8
Mohamedamine <khoubach44@gmail.com>
2023-01-15 18:31:28 +0100

    .

--- views/404.php
+++ views/404.php
@@ -9,7 +9,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Page Not Found - Brand</title>
-    <link rel="stylesheet" href="assets404/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="views/assets404/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
 </head>
 
@@ -17,7 +17,7 @@
 <section class="py-5 mt-5">
     <div class="container">
         <div class="row row-cols-1 d-flex justify-content-center align-items-center">
-            <div class="col-md-10 text-center"><img class="img-fluid w-100" src="assets404/img/illustrations/404.svg"></div>
+            <div class="col-md-10 text-center"><img class="img-fluid w-100" src="views/assets404/img/illustrations/404.svg"></div>
             <div class="col text-center">
                 <h2 class="display-3 fw-bold mb-4">La page web, n'a pas été trouvée</h2>
             </div>
@@ -25,8 +25,8 @@
     </div>
 </section>
 <footer></footer>
-<script src="assets404/bootstrap/js/bootstrap.min.js"></script>
-<script src="assets404/js/startup-modern.js"></script>
+<script src="views/assets404/bootstrap/js/bootstrap.min.js"></script>
+<script src="views/assets404/js/startup-modern.js"></script>
 </body>
 
 </html>
