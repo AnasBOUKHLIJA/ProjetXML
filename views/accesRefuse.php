f1891149d788414f45c907f1054518b580796bd9
anasBOUKHLIJA <anasboukhlijapro@gmail.com>
2023-01-15 18:44:55 +0100

    last commit

--- views/accesRefuse.php
+++ views/accesRefuse.php
@@ -1,3 +1,32 @@
 <?php
 
-echo "accès refusé";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Page Not Found - Brand</title>
+    <link rel="stylesheet" href="views/assets404/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
+</head>
+
+<body>
+<section class="mt-5">
+    <div class="container">
+        <div class="row row-cols-1 d-flex justify-content-center align-items-center">
+            <div class="col-md-10 text-center"><img class="img-fluid w-75 h-75 overflow-hidden" src="/ProjetXML/views/ourAssets/images/accessdenied.png"></div>
+            <div class="col text-center">
+                <h2 class="display-3 fw-bold mb-4">Accès refusé</h2>
+            </div>
+        </div>
+    </div>
+</section>
+<footer></footer>
+<script src="views/assets404/bootstrap/js/bootstrap.min.js"></script>
+<script src="views/assets404/js/startup-modern.js"></script>
+</body>
+
+</html>
