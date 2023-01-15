06170d0fa277310486ae55a3942abadca26bbd75
Mohamedamine <khoubach44@gmail.com>
2023-01-15 18:24:26 +0100

    .

--- views/404.php
+++ views/404.php
@@ -1,3 +1,27 @@
 <?php
 
-echo '<h1>Page not found</h1>';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Page Not Found - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+<section class="py-5 mt-5">
+    <div class="container">
+        <div class="row row-cols-1 d-flex justify-content-center align-items-center">
+            <div class="col-md-10 text-center"><img class="img-fluid w-100" src="views/assets/img/404.svg"></div>
+            <div class="col text-center"><h2 class="display-3 fw-bold mb-4">la page Web, n'a pas été trouvée !</h2>
+            </div>
+        </div>
+    </div>
+</section>
+<footer></footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
