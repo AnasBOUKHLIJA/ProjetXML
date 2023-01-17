0e44736e1f126fc39d09736d65bc0519e79a1675
anasBOUKHLIJA <anasboukhlijapro@gmail.com>
2023-01-17 14:29:55 +0100

    last comm it

--- views/includes/Toast.php
+++ views/includes/Toast.php
@@ -3,28 +3,32 @@
         <div id="toast-success">
             <img src="/ProjetXML/views/ourAssets/images/toast-save.png" />
             <span>
-                L'ajout est effectué avec succés
+                <!--L'ajout est effectué avec succés-->
+                <?php echo LangueController::get($_COOKIE['langue'],'toast')[0] ?>
             </span>
         </div>
     <?php } elseif (isset($_GET['info'])) { ?>
         <div id="toast-info">
             <img src="/ProjetXML/views/ourAssets/images/toast-info.png" />
             <span>
-                les modifications ont été enregistrés
+                <!--les modifications ont été enregistrés-->
+                <?php echo LangueController::get($_COOKIE['langue'],'toast')[1] ?>
             </span>
         </div>
     <?php } elseif (isset($_GET['attention'])) { ?>
         <div id="toast-warning">
             <img src="/ProjetXML/views/ourAssets/images/toast-warning.png" />
             <span>
-                La suppression est effectuée avec succés
+                <!--La suppression est effectuée avec succés-->
+                <?php echo LangueController::get($_COOKIE['langue'],'toast')[2] ?>
             </span>
         </div>
     <?php } elseif (isset($_GET['erreur'])) { ?>
         <div id="toast-error">
             <img src="/ProjetXML/views/ourAssets/images/toast-error.png" />
             <span>
-                Les informations ne sont pas enregistrées
+              <!--Les informations ne sont pas enregistrées-->
+                <?php echo LangueController::get($_COOKIE['langue'],'toast')[3] ?>
             </span>
         </div>
     <?php } ?>
